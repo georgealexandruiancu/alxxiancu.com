13d3c5bbd686cca85027c6c56d410ba7db71a61f
georgealexandruiancu <iancugeorgealexandru@gmail.com>
2020-03-31 19:47:12 +0300

    back-up

--- back-end/router/blog.js
+++ back-end/router/blog.js
@@ -1,9 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
-
 const router = express.Router();
-
 var mysql = require("mysql");
 
 var con = mysql.createPool({
@@ -25,7 +23,9 @@ router.post("/add", (req, res, next) => {
 	let path = data.path;
 	let tags = data.tags;
 
-	con.getConnection(err => {
+	var sql = "INSERT INTO blog (photo, short_description, body, title, path, tags) VALUES ("+ con.escape(photo) +","+ con.escape(shortDescription) +","+ con.escape(body) +","+ con.escape(title) +","+ con.escape(path) +","+ con.escape(tags) +")";
+
+	con.query(sql, (err, result) => {
 		if (err) {
 			console.log(err);
 			const error = new Error("It's seems to be a problem with database");
@@ -33,20 +33,9 @@ router.post("/add", (req, res, next) => {
 			return next(error);
 		}
 
-		var sql = "INSERT INTO blog (photo, short_description, body, title, path, tags) VALUES ('"+ photo +"','"+ shortDescription +"',"+ con.escape(body) +",'"+ title +"','"+ path +"','"+ tags +"')";
-
-		con.query(sql, (err, result) => {
-			if (err) {
-				console.log(err);
-				const error = new Error("It's seems to be a problem with database");
-				error.httpStatusCode = 400;
-				return next(error);
-			}
-
-			if (result) {
-				res.sendStatus(200);
-			}
-		})
+		if (result) {
+			res.sendStatus(200);
+		}
 	})
 });
 
@@ -60,7 +49,10 @@ router.put("/update", (req, res, next) => {
 	let path = req.body.path;
 	let tags = req.body.tags;
 
-	con.getConnection(err => {
+
+	var sql = "UPDATE blog SET photo = " + con.escape(photo) + ", short_description = " + con.escape(shortDescription) + ", body = " + con.escape(body) +", title = " + con.escape(title) +", path = " + con.escape(path) +", tags = " + con.escape(tags) + " WHERE id = '" + id +"'";
+
+	con.query(sql, (err, result) => {
 		if (err) {
 			console.log(err);
 			const error = new Error("It's seems to be a problem with database");
@@ -68,28 +60,19 @@ router.put("/update", (req, res, next) => {
 			return next(error);
 		}
 
-		var sql = "UPDATE blog SET photo = '" + photo + "', short_description = '" + shortDescription + "', body = '" + body +"', title = '" + title +"', path = '" + path +"', tags = '" + tags + "' WHERE id = '" + id +"'";
-
-		con.query(sql, (err, result) => {
-			if (err) {
-				console.log(err);
-				const error = new Error("It's seems to be a problem with database");
-				error.httpStatusCode = 400;
-				return next(error);
-			}
-
-			if (result) {
-				res.sendStatus(200);
-			}
-		})
-	})
+		if (result) {
+			res.sendStatus(200);
+		}
+	});
 });
 
 router.delete("/delete", (req, res, next) => {
 
 	let id = req.body.id;
 
-	con.getConnection(err => {
+	var sql = "DELETE FROM blog WHERE id='" + id + "'";
+
+	con.query(sql, (err, result) => {
 		if (err) {
 			console.log(err);
 			const error = new Error("It's seems to be a problem with database");
@@ -97,20 +80,9 @@ router.delete("/delete", (req, res, next) => {
 			return next(error);
 		}
 
-		var sql = "DELETE FROM blog WHERE id='" + id + "'";
-
-		con.query(sql, (err, result) => {
-			if (err) {
-				console.log(err);
-				const error = new Error("It's seems to be a problem with database");
-				error.httpStatusCode = 400;
-				return next(error);
-			}
-
-			if (result.affectedRows) {
-				res.sendStatus(200);
-			}
-		})
+		if (result.affectedRows) {
+			res.sendStatus(200);
+		}
 	})
 });
 
@@ -118,7 +90,9 @@ router.get("/get-post/:id/:path", (req, res, next) => {
 	let id = req.params.id;
 	let path = req.params.path;
 
-	con.getConnection(err => {
+	var sql = "SELECT * FROM blog WHERE id='" + id + "' AND path=" + con.escape(path);
+
+	con.query(sql, (err, result) => {
 		if (err) {
 			console.log(err);
 			const error = new Error("It's seems to be a problem with database");
@@ -126,24 +100,16 @@ router.get("/get-post/:id/:path", (req, res, next) => {
 			return next(error);
 		}
 
-		var sql = "SELECT * FROM blog WHERE id='" + id + "' AND path='" + path + "'";
-
-		con.query(sql, (err, result) => {
-			if (err) {
-				console.log(err);
-				const error = new Error("It's seems to be a problem with database");
-				error.httpStatusCode = 400;
-				return next(error);
-			}
-
-			res.send(JSON.parse(JSON.stringify(result)));
-		});
-	})
+		res.send(JSON.parse(JSON.stringify(result)));
+	});
 });
 
 
 router.get("/", (req, res, next) => {
-	con.getConnection(err => {
+
+	var sql = "SELECT * FROM blog ORDER BY id DESC";
+
+	con.query(sql, (err, result) => {
 		if (err) {
 			console.log(err);
 			const error = new Error("It's seems to be a problem with database");
@@ -151,19 +117,8 @@ router.get("/", (req, res, next) => {
 			return next(error);
 		}
 
-		var sql = "SELECT * FROM blog ORDER BY id DESC";
-
-		con.query(sql, (err, result) => {
-			if (err) {
-				console.log(err);
-				const error = new Error("It's seems to be a problem with database");
-				error.httpStatusCode = 400;
-				return next(error);
-			}
-
-			res.send(JSON.parse(JSON.stringify(result)));
-		});
-	})
+		res.send(JSON.parse(JSON.stringify(result)));
+	});
 });
 
 module.exports = router;
